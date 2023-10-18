361c949aa589ef84ad8a385728b6039d2a97df76
Vishwadeepsinh Gohil <gohilvishwadeepsinh2001@gmail.com>
2023-10-18 10:04:15 +0530

    #Some more components added

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const el = document.getElementById("root");
+const root = ReactDom.createRoot(el);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+    <ToastContainer
+        autoClose={1200}
+    />
+    <App/>
+    </BrowserRouter>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
