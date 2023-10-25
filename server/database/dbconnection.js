795ec1b30329a961436c5ebaaf2f05e6dac9a2cd
Vishwadeepsinh Gohil <gohilvishwadeepsinh2001@gmail.com>
2023-10-25 17:42:16 +0530

    Further components and API keys

--- server/database/dbconnection.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const dbconnect=async()=>{
-    mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
-        .then(() => console.log("Mongodb started")) 
-        .catch((err) => console.log(err))
-
-}
-
--- server/routes/paymentRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { initOrder, verifyOrder } from "../controllers/paymentController.js";
-
-const router=express.Router();
-
-router.post("/order", initOrder);
-router.post('/verify',verifyOrder);
-
-export default router;
