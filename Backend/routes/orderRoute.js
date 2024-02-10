614382e8ef51da26e2b5430191ec245278c95901
Vishwadeepsinh Gohil <gohilvishwadeepsinh2001@gmail.com>
2024-02-10 17:06:54 +0530

    Backend Updated

---
+++ Backend/routes/orderRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { acceptOrder, addOrder,getOrderByUserId, cancleOrder, deliveryOrder, getOrders,getOrderByFuelStationId} from "../controllers/orderController.js";
+//import { verifyToken } from "../middleware/auth.js";
+const router=express.Router();
+
+router.route('/')
+    .post(addOrder)
+    .get(getOrders)
+
+router.get('/getOrderByFuelStationId/:id',getOrderByFuelStationId);
+router.get('/getOrderByUserId/:id',getOrderByUserId);
+
+
+router.put('/cancel',cancleOrder);
+router.put('/accept',acceptOrder);
+router.put('/deliever',deliveryOrder);
+
+export default router;
